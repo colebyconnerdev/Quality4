0b5cccfc1926b4e8a952596ba8dbebb49e6a25ff
Coleby <coleby.conner.dev@gmail.com>
2016-07-16 21:03:38 -0500

    Initial commit

---
+++ app/src/main/java/com/ccdev/quality/Ignore/AuthSettings.java
@@ -0,0 +1,14 @@
+package com.ccdev.quality.Ignore;
+
+/**
+ * Created by Coleby on 7/6/2016.
+ */
+
+public class AuthSettings {
+    public static String SECRET_KEY = "zapUF6WnFloXgyk54Fu6wmwSU005JYsqoeoirP29QePSgSnigDuFnqrvWHP6Tn2";
+    public static String DOMAIN = "aie.local";
+    public static String SERVER = "10.10.0.20";
+    public static String ROOT = "public/quality/app/test";
+    public static String USERNAME = "coleby.conner";
+    public static String PASSWORD = "timp4AIE!1";
+}
